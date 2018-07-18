ab68afb7bc577c47c775580e0093d40d7f751bcd
shenwansheng <shenwansheng@inspiry.cn>
2018-07-18 16:42:45 +0800

    update code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 var dict = {};
-var systemLang = navigator.language.toLowerCase().slice(0,2);
+var systemLang = navigator.language.toLowerCase().slice(0, 2);
 $(function () {
   registerWords();
-  switch(getCookieVal("lang")) {
-    case "en" :
+  switch (getCookieVal("lang")) {
+    case "en":
       setLanguage("en");
       break
-    case "zh" :
+    case "zh":
       setLanguage("zh");
       break
     default:
       setLanguage(systemLang);
   }
-  
-// 切换语言事件
+
+  // 切换语言事件
   $("#enBtn").bind("click", function () {
     setLanguage("en");
   });
@@ -49,13 +49,13 @@ function setCookie(cookie) {
   var Days = 30; //此 cookie 将被保存 30 天
   var exp = new Date(); //new Date("December 31, 9998");
   exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-  document.cookie = cookie+ ";expires=" + exp.toGMTString();
+  document.cookie = cookie + ";expires=" + exp.toGMTString();
 }
 
 function translate(lang) {
-  if(sessionStorage.getItem(lang + "Data") != null){
+  if (sessionStorage.getItem(lang + "Data") != null) {
     dict = JSON.parse(sessionStorage.getItem(lang + "Data"));
-  }else{
+  } else {
     loadDict();
   }
 
@@ -68,6 +68,9 @@ function translate(lang) {
         $(this).text(__tr($(this).attr("lang")));
     }
   });
+  $("[lang-input]").each(function () {
+    $(this).attr("placeholder", __tr($(this).attr("lang-input")));
+  });
 }
 
 function __tr(src) {
@@ -79,7 +82,7 @@ function loadDict() {
   $.ajax({
     async: false,
     type: "GET",
-    url: "/lang/"+lang + ".json",
+    url: "/lang/" + lang + ".json",
     success: function (msg) {
       dict = msg;
       sessionStorage.setItem(lang + 'Data', JSON.stringify(dict));
@@ -96,6 +99,11 @@ function registerWords() {
         break;
       default:
         $(this).attr("lang", $(this).text());
+    }
+  });
+  $("[lang-input]").each(function () {
+    if ($(this).attr("lang-input") === "") {
+      $(this).attr("lang-input", $(this).attr("placeholder"));
     }
   });
 }
